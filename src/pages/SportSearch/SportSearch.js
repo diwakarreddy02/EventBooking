b8128ad857ebd8da421c212442fa43e040d73af1
Jadhav, Rahul Shrikant <rajadhav@iu.edu>
2023-04-08 21:42:06 -0400

    Update SportSearch.js

--- src/pages/SportSearch/SportSearch.js
+++ src/pages/SportSearch/SportSearch.js
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import NavbarSportSearch from "../../components/NavbarSportSearch/NavbarSportSearch";
 import { fetchAllVenues } from "../../services/SportService";
 import Card from "react-bootstrap/Card";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function SportSearch() {
   const [allSportsData, setAllSportsData] = useState([]);
   useEffect(() => {
     fetchAllVenues()
       .then((res) => {
+        console.log(res);
         setAllSportsData(res);
       })
       .catch((err) => {
@@ -20,23 +20,15 @@ export default function SportSearch() {
   return (
     <div>
       <NavbarSportSearch />
-      <div className="card-grid">
-        {allSportsData.map((element, index) => (
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Img variant="top" src="src/assets/baseball.jpg" />
-              <Card.Title>{element.id}</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                {element.Capacity}
-              </Card.Subtitle>
-              <Card.Text>{element.Description}</Card.Text>
-
-              <Link to="/Booking">
-                <Button variant="primary">BOOK NOW</Button>
-              </Link>
-            </Card.Body>
-          </Card>
-        ))}
+      <div className="col-10">
+        <ListGroup>
+          {allSportsData.map((element, index) => (
+            <ListGroup.Item>
+              <p>{element.Venue_Name}</p>
+              <p>{element.Description}</p>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
       </div>
     </div>
   );
